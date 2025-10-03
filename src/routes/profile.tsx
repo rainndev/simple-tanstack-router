9069b333ae2587005a10f8b54491e896622e618d
rainndev <182114243+rainndev@users.noreply.github.com>
2025-10-03 09:15:37 +0800

    feat: add react-icons dependency and enhance profile route with edit and more icons

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { MdEdit } from "react-icons/md";
+import { IoIosMore } from "react-icons/io";
 
 export const Route = createFileRoute("/profile")({
   component: RouteComponent,
@@ -6,9 +8,21 @@ export const Route = createFileRoute("/profile")({
 
 function RouteComponent() {
   return (
-    <div className="p-5 px-8 bg-white/10 rounded-xl no-underline space-y-2">
-      <h1 className="text-3xl">John Doe</h1>
-      <p className="text-xl">johndoe@gmail.com</p>
+    <div className="p-10 bg-white/10 rounded-xl no-underline space-y-2 items-center flex flex-col">
+      <h1 className="text-5xl text-amber-50 font-bold">John Doe</h1>
+      <p className="text-xl text-amber-50">johndoe@gmail.com</p>
+      <p className="text-xl text-amber-100">
+        Aspiring full stack web developer.
+      </p>
+
+      <div className="flex gap-2 mt-5">
+        <div className="p-2.5 border rounded-xl cursor-pointer">
+          <MdEdit className="size-5" />
+        </div>
+        <div className="p-2.5 border rounded-xl cursor-pointer">
+          <IoIosMore className="size-5" />
+        </div>
+      </div>
     </div>
   );
 }
